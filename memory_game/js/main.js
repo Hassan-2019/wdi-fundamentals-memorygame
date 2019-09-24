f65dacdc03263c6dfbd5804e2be04fd896aa7fe9
HassanAlkhari <alkeryhassan@gmail.com>
2019-09-24 21:21:40 +0300

    last commit

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,30 @@
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 console.log ("user flipped" + " " + cardOne);
 console.log ("user flipped" + " " + cardThree);
+*/
+ var cards = ["queen", "queen", "king", "king" ];
+ var cardsInPlay = [];
+ var cardOne = cards[0];
+  cardsInPlay.push(cardOne);
+  console.log("User flipped queen");
+  var cardTwo = cards[2];
+  cardsInPlay.push(cardTwo);
+  console.log("User flipped king");
+ if (cardsInPlay.length === 3 ) {
+  alert("You found a match!");
+
+ } else if (cardsInPlay[0] === cardsInPlay[2]) {
+
+alert("You found a match!");
+
+ } else {
+
+alert("Sorry, try again.");
+
+ }
+
+ 
